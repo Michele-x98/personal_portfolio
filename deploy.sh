32758dc6af88c99bec00c574036258fcbf62e339
MicheleBenedetti <mik-x98@hotmail.com>
2025-02-05 14:09:36 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 # copy all the content of the build/web folder into the github.io folder
 rps build
-cp -r build/web/* /Users/michelebenedetti/Documents/Lavoro/Michele-x98.github.io
-cd /Users/michelebenedetti/Documents/Lavoro/Michele-x98.github.io
+cp -r build/web/* /Users/michelebenedetti/Documents/Work/Michele-x98.github.io
+cd /Users/michelebenedetti/Documents/Work/Michele-x98.github.io
 git add .
-git commit -m "deploy"
+# get current date
+now=$(date)
+git commit -m "deploy $now"
 git push
 echo "Deployed successfully!"
 # open michelebenediti.it
